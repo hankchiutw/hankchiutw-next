02dcbeaa647af113090f5ab388a1470f2ea3b50d
hank <hankchiu.tw@gmail.com>
2025-02-19 15:25:36 +0800

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Hank Chiu - Web Front-End Expert',
+  title: 'hankchiutw - Web Front-End Expert',
   description:
     'Personal website of Hank Chiu, a Web Front-End Expert specializing in modern web technologies.',
 };
@@ -18,6 +18,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
