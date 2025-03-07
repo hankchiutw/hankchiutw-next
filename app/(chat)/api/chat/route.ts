c36ca0ce80f883f44be420aa9f7f0397f757ae13
hank <hankchiu.tw@gmail.com>
2025-03-07 17:16:36 +0800

    refactor(chat): trace client IP address as userId

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { createChatAction } from '@/features/chat';
+import { NextRequest } from 'next/server';
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   return createChatAction(req);
 }
--- features/chat/create-chat-action.ts
+++ features/chat/create-chat-action.ts
@@ -1,13 +1,16 @@
 import { groq } from '@ai-sdk/groq';
 import { streamText } from 'ai';
+import { NextRequest } from 'next/server';
 
 import { getSystemPrompt } from './prompts';
 
 const model = groq(process.env.GROQ_MODEL as string);
 
-export async function createChatAction(req: Request) {
+export async function createChatAction(req: NextRequest) {
   const { messages } = await req.json();
   const systemPrompt = await getSystemPrompt();
+
+  const userId = req.ip || req.headers.get('x-forwarded-for') || 'Unknown';
 
   const result = streamText({
     model,
@@ -15,6 +18,9 @@ export async function createChatAction(req: Request) {
     system: systemPrompt,
     experimental_telemetry: {
       isEnabled: true,
+      metadata: {
+        userId,
+      },
     },
   });
 
